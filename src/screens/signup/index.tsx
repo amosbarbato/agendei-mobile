dbec5df5e0afaec5e2bd9075026c0a7387b822eb
Amos Barbato <amosbarbato@gmail.com>
2024-10-28 13:31:56 -0300

    feat: adding signup form with image and button

---
+++ src/screens/signup/index.tsx
@@ -0,0 +1,51 @@
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import logo from "@/assets/logo.png"
+import { styles } from "./style";
+import { useState } from "react";
+import { Button } from "@/components/button";
+
+export function Signup() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerLogo}>
+        <Image source={logo} style={styles.logo} />
+      </View>
+
+      <View>
+        <View style={styles.containerInput}>
+          <TextInput
+            placeholder="Nome"
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            placeholder="Email"
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            placeholder="Senha"
+            secureTextEntry={true}
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        <Button text="Criar Conta" />
+      </View>
+
+      <View style={styles.footer}>
+        <Text>Já tenho uma conta. </Text>
+        <TouchableOpacity>
+          <Text style={styles.footerLink}>Fazer login.</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
