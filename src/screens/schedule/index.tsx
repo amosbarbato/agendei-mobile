0094dedc7cc621ba683321a3db0ac0b2ac55c646
Amos Barbato <amosbarbato@gmail.com>
2024-10-28 20:05:36 -0300

    feat: creating Schedule screen with calendar and time picker

---
+++ src/screens/schedule/index.tsx
@@ -0,0 +1,50 @@
+import { ptBR } from "@/constants/calendar";
+import { Text, View } from "react-native";
+import { Calendar, CalendarProps, type DateData, LocaleConfig } from "react-native-calendars";
+import { styles, theme } from "./style";
+import { useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import { Button } from "@/components/button";
+
+LocaleConfig.locales["pt-BR"] = ptBR;
+LocaleConfig.defaultLocale = "pt-BR";
+
+export function Schedule() {
+  const [day, setDay] = useState<DateData>()
+  const [hour, setHour] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <Calendar
+        theme={theme}
+        style={styles.calendar}
+        onDayPress={setDay => {
+          console.log('selected day', setDay);
+        }}
+        markedDates={day && {
+          [day.dateString]: { selected: true }
+        }}
+        minDate={new Date().toDateString()}
+      />
+
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.textLabel}>Horário</Text>
+          <Picker
+            selectedValue={hour}
+            onValueChange={(value, index) => { setHour(value) }}
+            style={styles.picker}
+          >
+            <Picker.Item label="09:00" value="09:00" />
+            <Picker.Item label="10:00" value="10:00" />
+            <Picker.Item label="11:00" value="11:00" />
+          </Picker>
+        </View>
+
+        <View>
+          <Button text="Confirmar reserva" />
+        </View>
+      </View>
+    </View>
+  )
+}
---
+++ src/screens/schedule/style.ts
@@ -0,0 +1,38 @@
+import { color, text } from "@/constants/theme";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingTop: 10,
+    flex: 1
+  },
+  calendar: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderEndColor: color.ligthGray
+  },
+  content: {
+    padding: 20,
+    paddingTop: 40,
+    justifyContent: "space-between",
+    flex: 1
+  },
+  textLabel: {
+    fontSize: text.lg,
+    fontWeight: "500",
+    color: color.darkGray,
+  },
+  picker: {
+    backgroundColor: color.clean,
+    marginTop: 10,
+  }
+})
+
+export const theme = {
+  textMonthFontSize: 18,
+  monthTextColor: color.darkGray,
+  todayTextColor: color.alert,
+  selectedDayBackgroundColor: color.primary,
+  selectedDayTextColor: color.white,
+  arrowColor: color.darkGray
+}
