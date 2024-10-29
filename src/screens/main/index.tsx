50b8604e85dbdc8ee3c83baabe516189a1ed8448
Amos Barbato <amosbarbato@gmail.com>
2024-10-28 21:52:39 -0300

    feat: implementing main navigation with bottom tab navigator

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -1,11 +1,84 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Calendar } from "./calendar";
 import { Home } from "./home";
 import { Profile } from "./profile";
+import { NavigationContainer } from "@react-navigation/native";
+import { Image } from "react-native";
+import icon from "@/constants/icon";
+import { color } from "@/constants/theme";
+
+const Tab = createBottomTabNavigator()
 
 export function Main() {
   return (
-    // <Home />
-    // <Calendar />
-    <Profile />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerTitleAlign: "center",
+            headerTitle: () => {
+              return (
+                <Image
+                  source={icon.logo}
+                  style={{ width: 140, height: 32, }}
+                />
+              )
+            },
+            tabBarShowLabel: false,
+            tabBarIcon: ({ focused }) => {
+              return (
+                <Image
+                  source={icon.home}
+                  style={{ width: 45, height: 45, opacity: focused ? 1 : 0.3 }}
+                />
+              )
+            }
+          }}
+        />
+
+        <Tab.Screen
+          name="Calendar"
+          component={Calendar}
+          options={{
+            headerTitleAlign: "center",
+            headerTitle: "Minhas Reservas",
+            headerTintColor: `${color.primary}`,
+            headerShadowVisible: true,
+            tabBarShowLabel: false,
+            tabBarIcon: ({ focused }) => {
+              return (
+                <Image
+                  source={icon.calendar}
+                  style={{ width: 45, height: 45, opacity: focused ? 1 : 0.3 }}
+                />
+              )
+            }
+          }}
+        />
+
+        <Tab.Screen
+          name="profile"
+          component={Profile}
+          options={{
+            headerTitleAlign: "center",
+            headerTitle: "Meu Perfil",
+            headerTintColor: `${color.primary}`,
+            headerShadowVisible: true,
+            tabBarShowLabel: false,
+            tabBarIcon: ({ focused }) => {
+              return (
+                <Image
+                  source={icon.profile}
+                  style={{ width: 45, height: 45, opacity: focused ? 1 : 0.3 }}
+                />
+              )
+            }
+          }}
+        />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
