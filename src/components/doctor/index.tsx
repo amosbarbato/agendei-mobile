e1a5d064e5cc0d3570b1e32eb247f6cefa8b643a
Amos Barbato <amosbarbato@gmail.com>
2024-10-31 16:23:08 -0300

    feat: navigating from doctors to services

--- src/components/doctor/index.tsx
+++ src/components/doctor/index.tsx
@@ -5,18 +5,21 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
+import icon from "@/constants/icon"
 import { styles } from "./style";
 
 interface CardDoctorProps {
-  icon: ImageSourcePropType
+  iconDoctor: string
+  id_doctor: number
   name: string
   specialty: string
+  onPress: () => void
 }
 
-export function CardDoctor({ icon, name, specialty }: CardDoctorProps) {
+export function CardDoctor({ iconDoctor, name, specialty, onPress }: CardDoctorProps) {
   return (
-    <TouchableOpacity style={styles.doctor}>
-      <Image source={icon} style={styles.icon} />
+    <TouchableOpacity style={styles.doctor} onPress={onPress}>
+      <Image source={iconDoctor === "M" ? icon.male : icon.female} style={styles.icon} />
       <View>
         <Text style={styles.name}>{name}</Text>
         <Text style={styles.specialty}>{specialty}</Text>
--- src/screens/main/home/index.tsx
+++ src/screens/main/home/index.tsx
@@ -1,10 +1,16 @@
 import { FlatList, Text, View } from "react-native";
 import { doctors } from "@/constants/data";
 import { CardDoctor } from "@/components/doctor";
-import icon from "@/constants/icon";
 import { styles } from "./style";
 
-export function Home() {
+export function Home({ navigation }: any) {
+
+  function handleClick(id_doctor: number, name: string, specialty: string, icon: string) {
+    navigation.navigate("services")
+
+    console.log(id_doctor, name, specialty, icon)
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Agende os seus serviços médicos</Text>
@@ -15,9 +21,11 @@ export function Home() {
         renderItem={({ item }) => {
           return (
             <CardDoctor
-              icon={item.icon === "M" ? icon.male : icon.female}
+              id_doctor={item.id_doctor}
+              iconDoctor={item.icon} // M ou F
               name={item.name}
               specialty={item.specialty}
+              onPress={() => handleClick(item.id_doctor, item.name, item.specialty, item.icon)}
             />
           )
         }}
