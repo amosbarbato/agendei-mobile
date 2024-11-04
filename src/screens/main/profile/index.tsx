af742e17fed7949fe726cc487ada4a0e6f52d859
Amos Barbato <amosbarbato@gmail.com>
2024-11-04 17:38:24 -0300

    refactor: inserting in View component

--- src/screens/main/profile/index.tsx
+++ src/screens/main/profile/index.tsx
@@ -1,27 +1,28 @@
-import { Alert, Text, View } from "react-native";
-import { styles } from "./style";
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "@/contexts/auth";
-import api from "@/constants/api";
+import { Text, View } from "react-native";
 import { useProfile } from "@/hooks/useProfile";
 import { Button } from "@/components/button";
+import { styles } from "./style";
 
 export function Profile() {
   const { profile, logout } = useProfile()
 
   return (
     <View style={styles.profile}>
-      <View style={styles.container}>
-        <Text style={styles.label}>Nome</Text>
-        <Text style={styles.info}>{profile.name}</Text>
+      <View>
+        <View style={styles.container}>
+          <Text style={styles.label}>Nome</Text>
+          <Text style={styles.info}>{profile.name}</Text>
+        </View>
+
+        <View style={styles.container}>
+          <Text style={styles.label}>Email</Text>
+          <Text style={styles.info}>{profile.email}</Text>
+        </View>
       </View>
 
-      <View style={styles.container}>
-        <Text style={styles.label}>Email</Text>
-        <Text style={styles.info}>{profile.email}</Text>
+      <View style={styles.buttonContainer}>
+        <Button text="Logout" onPress={logout} />
       </View>
-
-      <Button text="Logout" onPress={logout} />
     </View>
   )
 }
