7ca570548bd463ecc6ad3c5bac279751df7237f3
Amos Barbato <amosbarbato@gmail.com>
2024-11-04 17:37:42 -0300

    refactor: implements interface extends Doctor

--- src/components/doctor/index.tsx
+++ src/components/doctor/index.tsx
@@ -1,22 +1,13 @@
-import {
-  Image,
-  type ImageSourcePropType,
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Doctor } from "@/utils/interfaces";
 import icon from "@/constants/icon"
 import { styles } from "./style";
 
-interface CardDoctorProps {
-  iconDoctor: string
-  id_doctor: number
-  name: string
-  specialty: string
+interface DoctorProps extends Doctor {
   onPress: () => void
 }
 
-export function CardDoctor({ iconDoctor, name, specialty, onPress }: CardDoctorProps) {
+export function CardDoctor({ icon: iconDoctor, name, specialty, onPress }: DoctorProps) {
   return (
     <TouchableOpacity style={styles.doctor} onPress={onPress}>
       <Image source={iconDoctor === "M" ? icon.male : icon.female} style={styles.icon} />
