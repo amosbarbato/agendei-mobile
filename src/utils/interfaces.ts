953054899376e1974b4cff0a6af95ac399c1162a
Amos Barbato <amosbarbato@gmail.com>
2024-11-04 16:13:46 -0300

    refactor: separating in interfaces props

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,33 @@
+export interface Doctor {
+  id_doctor: number;
+  name: string;
+  specialty: string;
+  icon: string;
+}
+
+export interface Service {
+  id_service: number;
+  description: string;
+  price: number;
+}
+
+export interface Appointment {
+  id_appointment: number;
+  service: string;
+  doctor: string;
+  specialty: string;
+  booking_date: string;
+  booking_hour: string;
+}
+
+export interface UserProfile {
+  name: string;
+  email: string;
+}
+
+export interface Booking {
+  id_doctor: number;
+  id_service: number;
+  booking_date: string;
+  booking_hour: string;
+}
